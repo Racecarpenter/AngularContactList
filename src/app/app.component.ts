c8c9c48e9eb010e888141ca1c1b58d564b2a9e31
Race Carpenter <racecarpenter@gmail.com>
2017-08-17 09:10:17 -0700

    working contact list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  contacts = [
+    {
+      name: 'Jesus',
+      email: 'jesuscarpentry@bullshit.com',
+      phone: '4805325867'
+    },
+    {
+      name: 'Shitball Jones',
+      email: 'shitball@herbalife.com',
+      phone: '3244322432'
+    },
+    {
+      name: 'Habitual Line Stepper',
+      email: 'thelovingmonkey@dumb.com',
+      phone: '7777777777'
+    }
+  ];
+  addContact(name, email, phone) {
+    this.contacts.unshift({name, email, phone})
+  }
 }
